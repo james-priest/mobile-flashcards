cd24ae43efff89cb1cb45fed140c9ecb5fefd2c3
James Priest <james-priest@outlook.com>
2019-05-23 07:56:22 -0700

    fix: Update formatting

--- App.js
+++ App.js
@@ -16,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
